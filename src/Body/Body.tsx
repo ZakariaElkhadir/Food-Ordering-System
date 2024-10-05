03db202622e4ead8e615791672de3418a42e6c1b
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-10-05 23:32:20 +0100

    Refactor import statements and update dependencies

--- src/Body/Body.tsx
+++ src/Body/Body.tsx
@@ -1,12 +1,15 @@
 import About from "../About/About";
 import CardItem from "../Cards/CardItem";
 import HeroSection from "../HeroSection/HeroSection";
+
 const Body = () =>{
     return (
         <div>
             <HeroSection/>
             <CardItem/>
-            <About/>
+            <div id="about-section">
+                <About/>
+            </div>
         </div>
     )
 }
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,4 +1,5 @@
 import './Header.scss'
+import { HashLink as Link } from 'react-router-hash-link'
 function Header() {
     return(
         <>
@@ -8,7 +9,7 @@ function Header() {
                 <ul className='nav-links'>
                   <li><a href="#home">Home</a></li>
                   <li><a href="#menu">Services</a></li>
-                  <li><a href="#about">About</a></li>
+                  <li> <Link to="/#about-section">About</Link></li>
                   <li><a href="#contact">Contact</a></li>
                 </ul>
                 </nav>
